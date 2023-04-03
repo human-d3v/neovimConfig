ec905f592592e40cad84e737a86556221a29c604
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-04-02 22:28:15 -0600

    First commit. My custom nvim config.

---
+++ nvim/lua/bryartConfig/packer.lua
@@ -0,0 +1,58 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  --add finder to nvim
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  --add colortheme to nvim
+  use({ 
+	  'rose-pine/neovim', 
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
+  --add treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}
+  )
+
+  --add plugins
+  use('nvim-treesitter/playground')
+  use('nvim-lua/plenary.nvim')
+  use('ThePrimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+ 
+  --add Lanugage Server Protocol (for autocomplete)
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {                                      -- Optional
+		  'williamboman/mason.nvim',
+		  run = function()
+			  pcall(vim.cmd, 'MasonUpdate')
+		  end,
+	  },
+	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},     -- Required
+	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	  {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+end)
