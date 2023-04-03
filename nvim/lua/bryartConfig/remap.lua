1a4da4c9faadbe326cae8dc23330ff206c6d2c4f
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-04-03 08:13:52 -0600

    Added  comment for further clarification

--- nvim/lua/bryartConfig/remap.lua
+++ nvim/lua/bryartConfig/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " " 
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+--changed leader character to the spacebar for faster navigation. Then I mapped pv (project view) to the :Ex command in vim. 
