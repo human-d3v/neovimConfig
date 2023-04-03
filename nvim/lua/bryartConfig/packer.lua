a634323acb65e1ce0736b5478b6da936cf4a340a
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-04-03 08:15:00 -0600

    Updated comments

--- nvim/lua/bryartConfig/packer.lua
+++ nvim/lua/bryartConfig/packer.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file is the boilerplate code provided by Packer on github. 
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
