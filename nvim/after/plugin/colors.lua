4f431d8679a2670cff59332fdb6e850a03c43012
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-04-25 08:50:05 -0600

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,4 +1,4 @@
-function ColorMyPencils(color)
+function goodbyeYellowBrickRoad(color) --removes the background of the current workspace. 
 	color = color or "oxocarbon"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
