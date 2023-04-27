1bf8a899b48695c2768d58c7933267d6df46ae33
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-04-26 20:23:24 -0600

    Update packer.lua
    Updated indentation

--- nvim/lua/btconfig/packer.lua
+++ nvim/lua/btconfig/packer.lua
@@ -14,7 +14,14 @@ return require('packer').startup(function(use)
   }
 
   --color scheme
-  use {'nyoom-engineering/oxocarbon.nvim'}
+  --use {'nyoom-engineering/oxocarbon.nvim'}
+  --use {'ray-x/aurora'}
+  use {
+	  "justinsgithub/oh-my-monokai.nvim",
+	  config = function()
+		  require("oh-my-monokai").setup()
+	  end
+  }
 
   --treesitter & treesitter playground
     --treesitter displays the Abstract Syntax Tree in a separate panel when walking through code. 
@@ -57,35 +64,35 @@ return require('packer').startup(function(use)
   }
 
   --this is an autoclosing bracket plugin
-  use {
-	  'abecodes/tabout.nvim',
-	  config = function()
-		  require('tabout').setup {
-			  tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-			  backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-			  act_as_tab = true, -- shift content if tab out is not possible
-			  act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-			  default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-			  default_shift_tab = '<C-d>', -- reverse shift default action,
-			  enable_backwards = true, -- well ...
-			  completion = true, -- if the tabkey is used in a completion pum
-			  tabouts = {
-				  {open = "'", close = "'"},
-				  {open = '"', close = '"'},
-				  {open = '`', close = '`'},
-				  {open = '(', close = ')'},
-				  {open = '[', close = ']'},
-				  {open = '{', close = '}'}
-			  },
-			  ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-			  exclude = {} -- tabout will ignore these filetypes
-		  }
-	  end,
-	  wants = {'nvim-treesitter'}, -- or require if not used so far
-	  after = {'nvim-cmp'} -- if a completion plugin is using tabs load it before
-  }
+	use {
+  		'abecodes/tabout.nvim',
+  		config = function()
+    		require('tabout').setup {
+    		tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+    		backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+    		act_as_tab = true, -- shift content if tab out is not possible
+    		act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+    		default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+    		default_shift_tab = '<C-d>', -- reverse shift default action,
+    		enable_backwards = true, -- well ...
+    		completion = true, -- if the tabkey is used in a completion pum
+    		tabouts = {
+      			{open = "'", close = "'"},
+      			{open = '"', close = '"'},
+      			{open = '`', close = '`'},
+      			{open = '(', close = ')'},
+      			{open = '[', close = ']'},
+      			{open = '{', close = '}'}
+    		},
+    		ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+    		exclude = {} -- tabout will ignore these filetypes
+	}
+  	end,
+		wants = {'nvim-treesitter'}, -- or require if not used so far
+		after = {'nvim-cmp'} -- if a completion plugin is using tabs load it before
+	}
 
-  --setting up my ide to learn to code in R
+  --setting up to code in R
   --Nvim-R
   use('jalvesaq/Nvim-R')
   --ncm-R
