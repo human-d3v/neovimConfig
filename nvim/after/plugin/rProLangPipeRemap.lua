977f32668936dae1394c2e8b7cb44e5c52bd88c0
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-05-03 10:06:00 -0600

    Created Filetype dependent remap.
    Used the autocmd function to schedule filetype remap for R programming language pipe operator.

---
+++ nvim/after/plugin/rProLangPipeRemap.lua
@@ -0,0 +1,12 @@
+--this file is meant to create remapping for the pipe (%>%) operator on R programming files. 
+--{lhs} => ">" 
+--{rhs) => " %>% "
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"r", "rnoweb"},
+    callback = funciton()
+      vim.schedule(function()
+        vim.keymap.set("i", ">", "<Esc>:normal! a %>% <Enter>", {buffer = true})
+        vim.keymap.set("i", ">>", ">", {buffer = true})
+      end)
+    end,
+})
