cccc74f2bf92cf5fa593866a5f120ede6c647602
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-05-16 12:46:56 -0600

    Update and rename pipeRemapRLang.lua to autocmdRemaps.lua

---
+++ nvim/after/plugin/autocmdRemaps.lua
@@ -0,0 +1,22 @@
+--this file is to create filetype specific remaps with buffers. 
+--For R programming language
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {"r","rnoweb"},
+	callback = function()
+		vim.schedule(function()
+			vim.keymap.set("i",">"," %>% ", {buffer = true})
+			vim.keymap.set("i",">>",">", {buffer = true})
+		end)
+	end,
+})
+
+--for JS and TS
+vim.api.nvim_create_autocmd("FileType",{
+	pattern = {"js","ts","javascript","typescript"},
+	callback = function()
+		vim.schedule(function()
+			vim.keymap.set("i",">","=>",{buffer=true})
+			vim.keymap.set("i",">>",">",{buffer=true})
+		end)
+	end,
+})
