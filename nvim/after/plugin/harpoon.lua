0e696133f47f737f13d50f3e09f80f65eba813e1
Bryar <108082973+BryarTop@users.noreply.github.com>
2023-05-23 16:52:48 -0600

    Updated keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -7,7 +7,7 @@ vim.keymap.set('n','<leader>a',mark.add_file)
 vim.keymap.set('n','<C-e>',ui.toggle_quick_menu)
 --with the menu onscreen, map functions for navigation
   vim.keymap.set('n','<C-h>',function() ui.nav_file(1) end) --h for first
-  vim.keymap.set('n','<C-t>',function() ui.nav_file(2) end) --t for second
+  vim.keymap.set('n','<C-g>',function() ui.nav_file(2) end) --g for second
   vim.keymap.set('n','<C-n>',function() ui.nav_file(3) end) --n for third
   vim.keymap.set('n','<C-s>',function() ui.nav_file(4) end) --s for last
   
