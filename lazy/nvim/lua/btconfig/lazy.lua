edf023204ae1a1619937d27728966c63d99d41aa
Bryar Topham <BryTop05@gmail.com>
2024-01-16 14:32:33 -0700

    Added kitty terminal support, markdown support, changed colorscheme

--- lazy/nvim/lua/btconfig/lazy.lua
+++ lazy/nvim/lua/btconfig/lazy.lua
@@ -98,6 +98,16 @@ local plugins = {
 		--colorscheme
 		{
 		'GustavoPrietoP/doom-themes.nvim'
+		},
+		--kitty conf files syntax highlighting
+		'fladson/vim-kitty',
+		--markdown preview
+
+		{
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			ft = { "markdown" },
+			build = function() vim.fn["mkdp#util#install"]() end,
 		}
 }
 
