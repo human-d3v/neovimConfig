1b2202f24c8a2e5cd975018e5251c69b0de4e04c
Bryar Topham <BryTop05@gmail.com>
2024-01-23 10:43:42 -0700

    changed the colorscheme back to papercolor.

--- lazy/nvim/after/plugin/colors.lua
+++ lazy/nvim/after/plugin/colors.lua
@@ -1,23 +1,9 @@
 function bgOpacity(color)
-	--color = color or "PaperColor"
-	color = "no-clown-fiesta"
+	color = color or "PaperColor"
 	vim.cmd.colorscheme(color)
 	vim.opt.background = "dark"
 	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end
-
-require("no-clown-fiesta").setup({
-  transparent = true, -- Enable this to disable the bg color
-  styles = {
-    -- You can set any of the style values specified for `:h nvim_set_hl`
-    comments = {},
-    keywords = {},
-    functions = {},
-    variables = {},
-    type = { bold = true },
-    lsp = { underline = true }
-  },
-})
 
 bgOpacity()
